50e24c2817e5f6f5695cc6e5c94e52fef495103f
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-10-02 23:55:49 -0300

    1 - Inversion Capitalization

---
+++ 1_InversionCapitalization/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+string prinT = InversionCapitalization("The life is great you need to know that");
+Console.WriteLine(prinT);
+Console.ReadLine();
+
+string InversionCapitalization(string strg)
+{
+    strg = strg.ToLower();
+
+    StringBuilder stringBuilder = new();
+
+    for (int j = strg.Length - 1; j > -1; j--)
+    {
+        stringBuilder.Append(strg[j]);
+    }
+
+    string stringInverted = stringBuilder.ToString();
+
+    string[] strgV = stringInverted.Split(' ');
+
+    for (int i = 0; i < strgV.Length; i++)
+    {
+        char[] charArray = strgV[i].ToCharArray();
+        charArray[0] = Char.ToUpper(charArray[0]);
+        strgV[i] = new string(charArray);
+    }
+
+    return string.Join(" ", strgV);
+}
