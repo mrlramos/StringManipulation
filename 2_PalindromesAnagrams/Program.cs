2ebecfb8a77a9ec4068fe7c672c5f8a702d3cd3d
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-10-03 21:54:49 -0300

    2 - Palindromes Anagrams

---
+++ 2_PalindromesAnagrams/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(IsPalindrome("radar"));
+Console.WriteLine(IsPalindrome("test"));
+
+Console.WriteLine(IsAnagram("listen", "silent"));
+Console.WriteLine(IsAnagram("listrn", "silent"));
+Console.WriteLine(IsAnagram("listen", "silrnt"));
+Console.WriteLine(IsAnagram("test", "banana"));
+
+Console.ReadLine();
+
+static bool IsPalindrome (string word)
+{
+    string invertedWord = string.Empty;
+
+    for (int i = word.Length - 1; i > -1; i--)
+    {
+        invertedWord += word[i];
+    }
+
+    if (invertedWord == word)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+static bool IsAnagram (string word, string word2) 
+{
+    if (word.Length == word2.Length)
+    {
+        char[] charArrayWord = word.ToCharArray();
+        char[] charArrayWord2 = word2.ToCharArray();
+        short count = 0;
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            for (int j = 0; j < word.Length; j++)
+            {
+                if (charArrayWord[i] == charArrayWord2[j])
+                {
+                    count += 1;
+                }
+            }
+        }
+
+        if (count == word.Length)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # StringManipulation
 Repository to practice some string manipulation algorithms
+
+
+1 - InversionCapitalization
+Exercise: 
+String Inversion and Capitalization
+
+Description:
+Write a function that takes a string as an argument and returns that string inverted and with the first letter of each word capitalized.
+
+For example:
+     Input: "hello world"
+     Output: "Dlrow Olleh"
+
+
+2 - PalindromesAnagrams
+Exercise: 
+Palindromes and Anagrams
+
+Part 1 - Palindrome:
+
+Write a function in C# that accepts a string as an argument and returns true if the string is a palindrome and false otherwise. A palindrome is a word, phrase, or any other sequence of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+
+For example, "madam", "level", and "radar" are palindromes.
+
+Part 2 - Anagram:
+
+Write another function in C# that accepts two strings and returns true if one string is an anagram of the other and false if it is not. An anagram is a word or phrase formed by rearranging the letters of another word or phrase.
+
+For example, "listen" and "silent" are anagrams.
